b9c184173b408c00ca579805196abe7fcbcec95c
aflazur <mdaflazurrahaman2022@gmail.com>
2026-03-05 17:52:48 +0600

    Project setup and Navbar Complete

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="flex-1">
+                    <h2 className="font-bold text-2xl">CS — Ticket System</h2>
+                </div>
+                <div className="flex gap-3 items-center ">
+                    <div className="">
+                        <ul className="flex gap-3 font-normal">
+                            <li>Home</li>
+                            <li>FAQ</li>
+                            <li>Changelog</li>
+                            <li>Blog</li>
+                            <li>Download</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <button className="btn font-semibold bg-gradient-to-r from-[#632EE3] to-[#9F62F2] ...  text-white">+ New Ticket</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
